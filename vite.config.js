5503cb5e39b461ea76198dad0359de68e43dd3a0
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-08-11 20:15:48 +0530

    optimizing

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
+    minify: "esbuild",
+    esbuild: {
+      drop: ["console", "debugger"],
     },
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'gsap'],
-          webgl: ['gl-matrix', 'ogl'],
+          vendor: ["react", "react-dom", "gsap"],
+          webgl: ["gl-matrix", "ogl"],
         },
       },
     },
   },
-})
+});
